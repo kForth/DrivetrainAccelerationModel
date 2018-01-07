4ab05622f6897588f1230c62491632591871f740
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-07 16:13:38 -0500

    add missing import

--- optimize.py
+++ optimize.py
@@ -67,6 +67,7 @@ if __name__ == "__main__":
         distance_at_time += [[e['sim_distance'] for e in model.data_points]]
 
     if plot:
+        from mpl_toolkits.mplot3d import Axes3D
         import matplotlib.pyplot as plt
         import numpy as np
 
