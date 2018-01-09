4e302ff6c31d9edbfd84668dd7e82596e3c2540f
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-08 19:49:05 -0500

    cleanup

--- examples/drivetrain_compare.py
+++ examples/drivetrain_compare.py
@@ -5,15 +5,14 @@ from model.motors import _775pro
 if __name__ == "__main__":
 
     model = DrivetrainModel(CIM(6), gear_ratio=12, robot_mass=68, wheel_diameter=6 * 0.0254)
-    model.calc()
 
-    model2 = DrivetrainModel(MiniCIM(6), gear_ratio=12, robot_mass=68, wheel_diameter=6 * 0.0254,
-                             max_dist=0, simulation_time=model.get_final('time'))
-    model2.calc()
+    model2 = DrivetrainModel(MiniCIM(6), gear_ratio=12, robot_mass=68, wheel_diameter=6 * 0.0254)
 
     model3 = DrivetrainModel(_775pro(8), gear_ratio=32, robot_mass=68, wheel_diameter=6 * 0.0254,
-                             motor_voltage_limit=10, motor_current_limit=40,
-                             max_dist=0, simulation_time=model.get_final('time'))
+                             motor_voltage_limit=10, motor_current_limit=40)
+
+    model.calc()
+    model2.calc()
     model3.calc()
 
     plot_models(model, model2, model3, elements_to_plot=('pos', 'vel', 'accel'))
--- examples/elevator_compare.py
+++ examples/elevator_compare.py
@@ -5,15 +5,15 @@ if __name__ == "__main__":
 
     model = ElevatorModel(motors=_775pro(4), gear_ratio=165, payload_mass=250, pulley_diameter=2 * 0.0254,
                           max_dist=2 * 12 * 0.0254)
-    model.calc()
 
     model2 = ElevatorModel(motors=CIM(6), gear_ratio=55, payload_mass=250, pulley_diameter=2 * 0.0254,
                            max_dist=2 * 12 * 0.0254)
-    model2.calc()
 
     model3 = ElevatorModel(motors=_775pro(8), gear_ratio=165, payload_mass=250, pulley_diameter=2 * 0.0254,
-                           motor_voltage_limit=10,
-                           max_dist=2 * 12 * 0.0254)
+                           motor_voltage_limit=10, max_dist=2 * 12 * 0.0254)
+
+    model.calc()
+    model2.calc()
     model3.calc()
 
-    plot_models(model, model2, model3, elements_to_plot=('pos', 'vel'))
+    plot_models(model, model2, model3, elements_to_plot=('pos', 'vel', 'current'))
