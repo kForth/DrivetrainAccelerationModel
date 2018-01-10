11d9c4dee9a61d1dedd374047f851e719650f265
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-10 12:59:13 -0500

    cleanup

--- examples/elevator_compare.py
+++ examples/elevator_compare.py
@@ -1,19 +1,15 @@
 from model import ElevatorModel, plot_models
-from model.motors import _775pro, CIM
+from model.motors import _775pro
 
 if __name__ == "__main__":
+    models = []
 
-    model = ElevatorModel(motors=_775pro(4), gear_ratio=165, payload_mass=250, pulley_diameter=2 * 0.0254,
-                          max_dist=2 * 12 * 0.0254)
+    models += [ElevatorModel(motors=_775pro(4), gear_ratio=60 / 12 * 60 / 20, payload_mass=10,
+                             pulley_diameter=2 * 0.0254, max_dist=2, motor_current_limit=30)]
 
-    model2 = ElevatorModel(motors=CIM(6), gear_ratio=55, payload_mass=250, pulley_diameter=2 * 0.0254,
-                           max_dist=2 * 12 * 0.0254)
+    models += [ElevatorModel(motors=_775pro(4), gear_ratio=90, payload_mass=160,
+                             pulley_diameter=2 * 0.0254, max_dist=2 * 12 * 0.0254, battery_voltage=12)]
 
-    model3 = ElevatorModel(motors=_775pro(8), gear_ratio=165, payload_mass=250, pulley_diameter=2 * 0.0254,
-                           motor_voltage_limit=10, max_dist=2 * 12 * 0.0254)
+    [model.calc() for model in models]
 
-    model.calc()
-    model2.calc()
-    model3.calc()
-
-    plot_models(model, model2, model3, elements_to_plot=('pos', 'vel'))
+    plot_models(*models, elements_to_plot=('pos', 'vel', 'voltage'))
--- examples/elevator_optimize.py
+++ examples/elevator_optimize.py
@@ -1,12 +1,21 @@
 from model import ElevatorModel
 from model.motors import _775pro
-from optimizer import Optimizer
+from optimizer import Optimizer, RatioGenerator
 
 if __name__ == "__main__":
-    model = ElevatorModel(motors=_775pro(2), gear_ratio=100, payload_mass=250, pulley_diameter=2 * 0.0254)
+    # # Power Cube Elevator
+    # model = ElevatorModel(motors=_775pro(4), gear_ratio=15, payload_mass=10, pulley_diameter=2 * 0.0254, motor_current_limit=30)
+    #
+    # op = Optimizer(min_ratio=2, max_ratio=30, ratio_step=0.5,
+    #                max_dist=3, distance_step=0.05 * 12 * 0.0254,
+    #                model=model)
 
-    op = Optimizer(min_ratio=100, max_ratio=300, ratio_step=5,
-                   max_dist=2 * 12 * 0.0254, distance_step=0.05 * 12 * 0.0254,
-                   model=model)
+    # Climbing Elevator
+    model = ElevatorModel(motors=_775pro(4), gear_ratio=120, payload_mass=160, pulley_diameter=2 * 0.0254)
+
+    ratio_generator = RatioGenerator(RatioGenerator.GEARS_32_DP, input_gears=(12,), max_stages=4, min_ratio=32, max_ratio=300)
+
+    op = Optimizer(model=model, ratio_generator=ratio_generator,
+                   max_dist=2 * 12 * 0.0254, distance_step=0.01 * 12 * 0.0254)
     op.run()
     op.save_xlsx('/tmp/optimize_elevator.xlsx')
